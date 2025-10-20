6642530afec49a0f3833addb65b2dcc947699bc7
Aaron <184453194+aeon45525@users.noreply.github.com>
2025-10-20 20:06:28 +0800

    function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3);
+    let ComputerChoice;
+
+    if (num === 0){
+        return ComputerChoice = "Rock"
+    }
+    else if (num === 1){
+        return ComputerChoice = "Paper"
+    }
+    else {
+        return ComputerChoice = "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
